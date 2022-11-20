f959f3418e4aa999626a63efaddf3d18759065ff
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2022-11-20 18:44:36 +0100

    feat(card): add card macro for easier dev

--- belote_libre_backend/src/card.rs
+++ belote_libre_backend/src/card.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 
 use strum_macros::EnumIter;
@@ -40,6 +41,38 @@ impl fmt::Display for Card {
     }
 }
 
+pub fn get_suit_shortcut() -> HashMap<String, Suit> {
+    HashMap::from([
+        ("H".to_string(), Suit::Heart),
+        ("D".to_string(), Suit::Diamond),
+        ("C".to_string(), Suit::Club),
+        ("S".to_string(), Suit::Spade),
+    ])
+}
+
+pub fn get_symbol_shortcut() -> HashMap<String, Symbol> {
+    HashMap::from([
+        ("7".to_string(), Symbol::Seven),
+        ("8".to_string(), Symbol::Height),
+        ("9".to_string(), Symbol::Nine),
+        ("10".to_string(), Symbol::Ten),
+        ("J".to_string(), Symbol::Jack),
+        ("Q".to_string(), Symbol::Queen),
+        ("K".to_string(), Symbol::King),
+        ("A".to_string(), Symbol::Ace),
+    ])
+}
+
+#[macro_export]
+macro_rules! card {
+    ($a:expr,$b:expr) => {{
+        Card::new(
+            get_suit_shortcut()[&$a.to_string()],
+            get_symbol_shortcut()[&$b.to_string()],
+        )
+    }};
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +81,13 @@ mod tests {
     fn init_card() {
         let _card = Card::new(Suit::Diamond, Symbol::Ten);
         println!("{}", _card);
+    }
+
+    #[test]
+    fn init_card_macro() {
+        let card = Card::new(Suit::Heart, Symbol::Seven);
+        let card2 = card!("H", "7");
+
+        assert!(card == card2);
     }
 }
